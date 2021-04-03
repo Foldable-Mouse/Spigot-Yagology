e972ecacc63801199743707170983d2572c921b9
Foldable-Mouse <81262222+Foldable-Mouse@users.noreply.github.com>
2021-04-03 12:53:18 -0400

    Add files via upload

---
+++ src/foldable/technique/yagology/ChestGUI.java
@@ -0,0 +1,280 @@
+package foldable.technique.yagology;
+
+import java.util.Arrays;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+/*================================([    CHESTGUI    ])===============================*/
+/*																					 */
+//                                       ./#%%%/*.                                   //
+//                               .@@@@@@@&       *@@@@                               //
+//                           /@@@@@@.                @@@(                            //
+//                        @@@%((@@@                    &@@.                          //
+//                      @@%(((((@@                      (@@(                         //
+//                    &@@(((((((@&     ,**/*           (@@@@.                        //
+//                   ,@&(((((((#@,    @@@@@@@@       @@@@@@@@                        //
+//                   @@((((((((@@     @@@@@@@@       @@@@@@@@                        //
+//                   @@((((((((@@                         ,@@                        //
+//                   @@#((((((((@%                         @@                        //
+//                    @@((((((((@@             @@@@@      .@@                        //
+//                    @@#(((((((#@@            @@@@@      %@@                        //
+//                     &@#((((((((@@/                     @@*                        //
+//                      @@((((((((#(@@#                  (@@                         //
+//                      @@((((((((((((%@@#               @@@                         //
+//                      @@@((((((((((((@@@@@@@@@@@@@&@@@@@@                          //
+//                      @@@((((((((((#@@*.@@@#((((((((((@@.                          //
+//                      @@(((((((((((@@    @@&((((((((((%@                           //
+//                     %@%((((((((((@@      @@(((((((((((@@(                         //
+//                    (@@(((((((((&@@        @@#((((((((((@@                         //
+//                  ,@@(((((((((@@/          @@((((((((((#@@                         //
+//                  @@%((((((((@@&            &@@((((((((((@@                        //
+//                @@@((((((((&@@               @@@((((((((((@*                       //
+//              @@@@%(((#(#@@@&                 @@@((((((##@@@@@@&%.                 //
+//            .@@@@@@@@@@@@@@@@@@@@@@           @@@@@@@@@@@@@@@@&&@@@@@@             //
+//            @@%                   @@@         @@@/                 .@@@            //
+//            @@@.                  .@@@         @@@*               #@@@.            //
+//             .@@@@@@@@@@@@@@@@@@@@@@.           %@@@@@@@@@@@@@@@@@@(               //
+/*																				 	 */
+/*===================================================================================*/
+public class ChestGUI implements Listener
+{
+	public final Inventory inv;
+	public Inventory recipeInv;
+	public ChestGUI()
+	{
+		inv = Bukkit.createInventory(null,  18, ChatColor.translateAlternateColorCodes('&', "&e&lYagology GUI"));
+		initializeItems();
+	}
+	public ChestGUI(Player p)
+	{
+		// Initialize inv, with nine slots:
+		inv = Bukkit.createInventory(p, 18, ChatColor.translateAlternateColorCodes('&', "&e&lYagology GUI"));
+		// Initializing the inventory (putting items in it)
+		initializeItems();
+	}
+	
+	public void initializeItems()
+	{
+		inv.addItem(YagItems.createYagHelmet());
+		inv.addItem(YagItems.createYagChestplate());
+		inv.addItem(YagItems.createYagLeggings());
+		inv.addItem(YagItems.createYagBoots());
+		inv.addItem(YagItems.createYaganeseSword());
+		inv.addItem(YagItems.createYaganesePickaxe());
+		inv.addItem(YagItems.createYaganeseAxe());
+		inv.addItem(YagItems.createYaganeseShovel());
+		inv.addItem(YagItems.createGucciBeltBuckle());
+		inv.addItem(YagItems.createYagCrossBow());
+		inv.addItem(YagItems.createYagNose());
+		inv.setItem(17, YagItems.createCreditBook());
+	}
+	
+	// Conveniently adds items on the spot to be added into the GUI. 
+	protected ItemStack createGuiItem(final Material material, final String name, final String... lore)
+	{
+		final ItemStack item = new ItemStack(material, 1);
+		final ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
+		meta.setLore(Arrays.asList(lore));
+		item.setItemMeta(meta);
+		return item;
+	}
+	
+	// Opens GUI inventory for player
+	public void openInventory(final HumanEntity ent)
+	{
+		ent.openInventory(inv);
+	}
+	// Opens recipe GUI for player
+	public void openRecipe(final HumanEntity ent)
+	{
+		ent.openInventory(recipeInv);
+	}
+	// Checks for clicking of the items. Event handler. 
+	@EventHandler
+	public void onInventoryClick(final InventoryClickEvent e)
+	{
+		if(e.getClickedInventory() == null) return;
+		if((e.getInventory().contains(YagItems.createCreditBook())) || (e.getInventory().contains(YagItems.createBackFeather())))
+		{
+			// Make sure that item selected is not null
+			final ItemStack clickedItem = e.getCurrentItem();
+			if(clickedItem == null || clickedItem.getType() == Material.AIR) return; 
+			
+			// Check what item was clicked, then open a new inventory for the recipe of clicked item. 
+			if(clickedItem.equals(YagItems.createYagHelmet()))
+			{
+				recipeInv = Bukkit.createInventory(e.getWhoClicked(), 27, ChatColor.translateAlternateColorCodes('&', "&e&lYaganese Helmet"));
+				
+				recipeInv.setItem(18, YagItems.createBackFeather());
+				
+				recipeInv.setItem(3, YagItems.createYagNose());
+				recipeInv.setItem(4, new ItemStack(Material.BREAD));
+				recipeInv.setItem(5, YagItems.createYagNose());
+				recipeInv.setItem(12, YagItems.createYagNose());
+				recipeInv.setItem(14, YagItems.createYagNose());
+				
+				openRecipe(e.getWhoClicked());
+			}
+			if(clickedItem.equals(YagItems.createYagChestplate()))
+			{
+				recipeInv = Bukkit.createInventory(e.getWhoClicked(), 27, ChatColor.translateAlternateColorCodes('&', "&e&lYaganese Chestplate"));
+				
+				recipeInv.setItem(18, YagItems.createBackFeather());
+				
+				recipeInv.setItem(3, YagItems.createYagNose());
+				recipeInv.setItem(5, YagItems.createYagNose());
+				recipeInv.setItem(12, YagItems.createYagNose());
+				recipeInv.setItem(13, new ItemStack(Material.BREAD));
+				recipeInv.setItem(14, YagItems.createYagNose());
+				recipeInv.setItem(21, YagItems.createYagNose());
+				recipeInv.setItem(22, YagItems.createYagNose());
+				recipeInv.setItem(23, YagItems.createYagNose());
+				
+				openRecipe(e.getWhoClicked());
+			}
+			if(clickedItem.equals(YagItems.createYagLeggings()))
+			{
+				recipeInv = Bukkit.createInventory(e.getWhoClicked(), 27, ChatColor.translateAlternateColorCodes('&', "&e&lYaganese Leggings"));
+				
+				recipeInv.setItem(18, YagItems.createBackFeather());
+				
+				recipeInv.setItem(3, new ItemStack(Material.BREAD));
+				recipeInv.setItem(4, YagItems.createGucciBeltBuckle());
+				recipeInv.setItem(5, new ItemStack(Material.BREAD));
+				recipeInv.setItem(12, YagItems.createYagNose());
+				recipeInv.setItem(14, YagItems.createYagNose());
+				recipeInv.setItem(21, YagItems.createYagNose());
+				recipeInv.setItem(23, YagItems.createYagNose());
+				
+				openRecipe(e.getWhoClicked());
+			}
+			if(clickedItem.equals(YagItems.createYagBoots()))
+			{
+				recipeInv = Bukkit.createInventory(e.getWhoClicked(), 27, ChatColor.translateAlternateColorCodes('&', "&e&lYaganese Boots"));
+				
+				recipeInv.setItem(18, YagItems.createBackFeather());
+				
+				recipeInv.setItem(12, new ItemStack(Material.LEATHER));
+				recipeInv.setItem(14, new ItemStack(Material.LEATHER));
+				recipeInv.setItem(21, YagItems.createYagNose());
+				recipeInv.setItem(23, YagItems.createYagNose());
+				
+				openRecipe(e.getWhoClicked());
+			}
+			if(clickedItem.equals(YagItems.createYaganeseSword()))
+			{
+				recipeInv = Bukkit.createInventory(e.getWhoClicked(), 27, ChatColor.translateAlternateColorCodes('&', "&e&lYaganese Sword"));
+				
+				recipeInv.setItem(18, YagItems.createBackFeather());
+				
+				recipeInv.setItem(4, YagItems.createYagNose());
+				recipeInv.setItem(13, YagItems.createYagNose());
+				recipeInv.setItem(22, new ItemStack(Material.BLAZE_ROD));
+				
+				openRecipe(e.getWhoClicked());
+			}
+			if(clickedItem.equals(YagItems.createYagNose()))
+			{
+				recipeInv = Bukkit.createInventory(e.getWhoClicked(), 27, ChatColor.translateAlternateColorCodes('&', "&d&lYaganese Nose"));
+				
+				recipeInv.setItem(18, YagItems.createBackFeather());
+				
+				recipeInv.setItem(3, new ItemStack(Material.DIAMOND));
+				recipeInv.setItem(4, new ItemStack(Material.BREAD));
+				recipeInv.setItem(5, new ItemStack(Material.DIAMOND));
+				recipeInv.setItem(12, new ItemStack(Material.BREAD));
+				recipeInv.setItem(13, new ItemStack(Material.NETHERITE_INGOT));
+				recipeInv.setItem(14, new ItemStack(Material.BREAD));
+				recipeInv.setItem(21, new ItemStack(Material.DIAMOND));
+				recipeInv.setItem(22, new ItemStack(Material.BREAD));
+				recipeInv.setItem(23, new ItemStack(Material.DIAMOND));
+				
+				openRecipe(e.getWhoClicked());
+			}
+			if(clickedItem.equals(YagItems.createYaganesePickaxe()))
+			{
+				recipeInv = Bukkit.createInventory(e.getWhoClicked(), 27, ChatColor.translateAlternateColorCodes('&', "&e&lYaganese Pickaxe"));
+				
+				// Setting the recipe to that of a Yaganese pickaxe, manually
+				recipeInv.setItem(18, YagItems.createBackFeather());
+				
+				recipeInv.setItem(3, YagItems.createYagNose());
+				recipeInv.setItem(4, YagItems.createYagNose());
+				recipeInv.setItem(5, YagItems.createYagNose());
+				recipeInv.setItem(13, new ItemStack(Material.BLAZE_ROD));
+				recipeInv.setItem(22, new ItemStack(Material.BLAZE_ROD));
+				
+				openRecipe(e.getWhoClicked());
+			}
+			if(clickedItem.equals(YagItems.createYaganeseAxe()))
+			{
+				recipeInv = Bukkit.createInventory(e.getWhoClicked(), 27, ChatColor.translateAlternateColorCodes('&', "&e&lYaganese Axe"));
+				
+				recipeInv.setItem(18, YagItems.createBackFeather());
+				
+				recipeInv.setItem(3, YagItems.createYagNose());
+				recipeInv.setItem(4, YagItems.createYagNose());
+				recipeInv.setItem(12, YagItems.createYagNose());
+				recipeInv.setItem(13, new ItemStack(Material.BLAZE_ROD));
+				recipeInv.setItem(22, new ItemStack(Material.BLAZE_ROD));
+				
+				openRecipe(e.getWhoClicked());
+			}
+			if(clickedItem.equals(YagItems.createYaganeseShovel()))
+			{
+				recipeInv = Bukkit.createInventory(e.getWhoClicked(), 27, ChatColor.translateAlternateColorCodes('&', "&e&lYaganese Shovel"));
+				
+				recipeInv.setItem(18, YagItems.createBackFeather());
+				
+				recipeInv.setItem(4, YagItems.createYagNose());
+				recipeInv.setItem(13, new ItemStack(Material.BLAZE_ROD));
+				recipeInv.setItem(22, new ItemStack(Material.BLAZE_ROD));
+				
+				openRecipe(e.getWhoClicked());
+			}
+			if(clickedItem.equals(YagItems.createGucciBeltBuckle()))
+			{
+				recipeInv = Bukkit.createInventory(e.getWhoClicked(), 27, ChatColor.translateAlternateColorCodes('&', "&a&lGucci Belt Buckle"));
+				
+				recipeInv.setItem(18, YagItems.createBackFeather());
+				
+				recipeInv.setItem(12, new ItemStack(Material.DIAMOND));
+				recipeInv.setItem(13, new ItemStack(Material.NETHERITE_INGOT));
+				recipeInv.setItem(14, new ItemStack(Material.DIAMOND));
+				
+				openRecipe(e.getWhoClicked());
+			}
+			if(clickedItem.equals(YagItems.createYagCrossBow()))
+			{
+				recipeInv = Bukkit.createInventory(e.getWhoClicked(), 27,  ChatColor.translateAlternateColorCodes('&', "&e&lYaganese Crossbow"));
+				
+				recipeInv.setItem(18, YagItems.createBackFeather());
+							recipeInv.setItem(3, new ItemStack(Material.BLAZE_ROD));
+				recipeInv.setItem(4, YagItems.createYagNose());
+				recipeInv.setItem(5, new ItemStack(Material.BLAZE_ROD));
+				recipeInv.setItem(12, new ItemStack(Material.STRING));
+				recipeInv.setItem(13, new ItemStack(Material.TRIPWIRE_HOOK));
+				recipeInv.setItem(14, new ItemStack(Material.STRING));
+				recipeInv.setItem(22, new ItemStack(Material.BLAZE_ROD));
+				
+				openRecipe(e.getWhoClicked());
+			}
+			if(clickedItem.equals(YagItems.createBackFeather()))
+			{
+				openInventory(e.getWhoClicked());
+			}
+			e.setCancelled(true);
+		}
+	}
+	
+}
---
+++ src/foldable/technique/yagology/Events.java
@@ -0,0 +1,113 @@
+package foldable.technique.yagology;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+/*================================([     EVENTS     ])===============================*/
+/*																					 */
+//                                       ./#%%%/*.                                   //
+//                               .@@@@@@@&       *@@@@                               //
+//                           /@@@@@@.                @@@(                            //
+//                        @@@%((@@@                    &@@.                          //
+//                      @@%(((((@@                      (@@(                         //
+//                    &@@(((((((@&     ,**/*           (@@@@.                        //
+//                   ,@&(((((((#@,    @@@@@@@@       @@@@@@@@                        //
+//                   @@((((((((@@     @@@@@@@@       @@@@@@@@                        //
+//                   @@((((((((@@                         ,@@                        //
+//                   @@#((((((((@%                         @@                        //
+//                    @@((((((((@@             @@@@@      .@@                        //
+//                    @@#(((((((#@@            @@@@@      %@@                        //
+//                     &@#((((((((@@/                     @@*                        //
+//                      @@((((((((#(@@#                  (@@                         //
+//                      @@((((((((((((%@@#               @@@                         //
+//                      @@@((((((((((((@@@@@@@@@@@@@&@@@@@@                          //
+//                      @@@((((((((((#@@*.@@@#((((((((((@@.                          //
+//                      @@(((((((((((@@    @@&((((((((((%@                           //
+//                     %@%((((((((((@@      @@(((((((((((@@(                         //
+//                    (@@(((((((((&@@        @@#((((((((((@@                         //
+//                  ,@@(((((((((@@/          @@((((((((((#@@                         //
+//                  @@%((((((((@@&            &@@((((((((((@@                        //
+//                @@@((((((((&@@               @@@((((((((((@*                       //
+//              @@@@%(((#(#@@@&                 @@@((((((##@@@@@@&%.                 //
+//            .@@@@@@@@@@@@@@@@@@@@@@           @@@@@@@@@@@@@@@@&&@@@@@@             //
+//            @@%                   @@@         @@@/                 .@@@            //
+//            @@@.                  .@@@         @@@*               #@@@.            //
+//             .@@@@@@@@@@@@@@@@@@@@@@.           %@@@@@@@@@@@@@@@@@@(               //
+/*																				 	 */
+/*===================================================================================*/
+public class Events implements Listener
+{	
+	@EventHandler
+	public void onPlayerDeathEvent(PlayerDeathEvent e)
+	{
+		// Handling the death of player by username Yaganese having chance to drop yag-nose
+		if(e.getEntity().getName().equals("Yaganese"))
+		{
+			int rand = (int) (Math.random() * 10000); // .1% chance that yaganese drops yagnose
+			if(rand == 0)
+			{
+				YagItems.dropYagNose(e.getEntity().getLocation(), e.getEntity());
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onPrepareItemEnchantEvent(PrepareItemEnchantEvent e) throws InterruptedException
+	{
+		// Handling the level 1000 ultimate YAGANESE BREAD enchanting process: 
+		if(e.getEnchanter().getExpToLevel() >= 1000) // If the player has at least 1000 levels
+		{
+			if(e.getItem() != null && (e.getItem().isSimilar(new ItemStack(Material.BREAD)))
+					|| e.getItem().isSimilar(new ItemStack(YagItems.createSemiYagBread(1)))) // If the item inside is "similar to" bread
+			{
+				e.getItem().setAmount(0); // Making the item being enchanted disappear. 
+				
+				e.getEnchanter().closeInventory();
+				e.getEnchanter().playSound(e.getEnchanter().getLocation(), Sound.ENTITY_ENDER_DRAGON_DEATH, 10, 10);
+				//Take the player's experience:
+				e.getEnchanter().giveExp(-4339720); // 4339720 is 1000 levels in xp. Works well enough for me. 
+				YagItems.dropYagBread(e.getEnchantBlock().getLocation(), e.getEnchanter());
+			}
+		}
+	}
+	
+	// Used to make YAG-BREAD fill entire hunger bar
+	// And to give special effects like a golden apple
+	@EventHandler
+	public void onFoodLevelChangeEvent(FoodLevelChangeEvent e)
+	{
+		if(e.getItem() != null && e.getItem().isSimilar(new ItemStack(YagItems.createYagBread())))
+		{
+			// If YAG BREAD is consumed:
+			e.setFoodLevel(20); // Gives full food
+			Collection<PotionEffect> effects = new ArrayList<PotionEffect>();
+			// Duration is stored as ticks
+			// Amplifier intensity starts at 0 
+			// PotionEffect(PotionEffectType type, int durationTicks, int amplifier)
+			// 18,000 ticks = 15 minutes
+			effects.add(new PotionEffect(PotionEffectType.ABSORPTION, 18000, 4));
+			effects.add(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 18000, 0));
+			effects.add(new PotionEffect(PotionEffectType.REGENERATION, 18000, 2));
+			effects.add(new PotionEffect(PotionEffectType.GLOWING, 18000, 0));
+			effects.add(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 18000, 4));
+			effects.add(new PotionEffect(PotionEffectType.FAST_DIGGING, 18000, 2));
+			e.getEntity().addPotionEffects(effects);
+			
+		}
+		if(e.getItem() != null && e.getItem().isSimilar(new ItemStack(YagItems.createSemiYagBread(1))))
+		{
+			// Makes weirdly crafted yag breads award more hunger
+			e.setFoodLevel(e.getFoodLevel() + 1);
+		}
+	}
+}
